23d6be0525bd4f9ed7f97c9a037e62b753d9956a
planethouki <26595148+planethouki@users.noreply.github.com>
2025-02-11 11:47:08 +0900

    functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,18 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import {setGlobalOptions} from "firebase-functions/v2";
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+setGlobalOptions({
+  region: "asia-northeast1",
+});
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
