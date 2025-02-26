baaa913f2b0bef9f69943bd916c4d14dcfbc3244
planethouki <26595148+planethouki@users.noreply.github.com>
2025-02-26 22:06:16 +0900

    sample api

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -1,9 +1,36 @@
 import * as express from "express";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
 
 const app = express();
 
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/home", async (req, res) => {
+  try {
+    const contests = await db
+      .collection("contests")
+      .orderBy("createdAt", "asc")
+      .limit(8)
+      .get()
+      .then((snapshot) => {
+        return snapshot.docs.map((doc) => doc.data());
+      });
+    res.status(200).json({success: true, contests});
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching data",
+      error,
+    });
+  }
 });
 
 export default app;
