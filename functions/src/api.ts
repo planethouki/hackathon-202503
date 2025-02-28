b9422ed490d5624fab68b1e04e7297d311ecad69
planethouki <26595148+planethouki@users.noreply.github.com>
2025-02-28 10:55:59 +0900

    home api details, faster

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -15,31 +15,76 @@ app.get("/hello", (req, res) => {
 
 app.get("/home", async (req, res) => {
   try {
-    const contests = await db
+    const contestsQuery = await db
       .collection("contests")
       .orderBy("createdAt", "desc")
       .limit(8)
-      .get()
-      .then((snapshot) => {
-        return snapshot.docs.map((doc) => doc.data());
-      });
-    const punchlines = await db
+      .get();
+    const punchlinesQuery = await db
       .collection("punchlines")
       .orderBy("createdAt", "desc")
       .limit(4)
-      .get()
-      .then((snapshot) => {
-        return snapshot.docs.map((doc) => doc.data());
-      });
-    const users = await db
+      .get();
+    const usersQuery = await db
       .collection("users")
       .orderBy("createdAt", "desc")
       .limit(16)
-      .get()
-      .then((snapshot) => {
-        return snapshot.docs.map((doc) => doc.data());
+      .get();
+
+    const [
+      contestsSnap,
+      punchlineSnap,
+      userSnap,
+    ] = await Promise.all([
+      contestsQuery,
+      punchlinesQuery,
+      usersQuery,
+    ]);
+
+    const contests = contestsSnap.docs.map((doc) => doc.data());
+    const punchlines = punchlineSnap.docs.map((doc) => doc.data());
+    const users = userSnap.docs.map((doc) => doc.data());
+
+    const contestsDetailsQuery = db
+      .collection("contests")
+      .where("id", "in", punchlines.map((p) => p.contestId))
+      .get();
+    const usersDetailsQuery = db
+      .collection("users")
+      .where("id", "in", punchlines.map((p) => p.userId))
+      .get();
+
+    const [
+      contestsDetails,
+      usersDetails,
+    ] = await Promise.all([contestsDetailsQuery, usersDetailsQuery])
+      .then(([c, u]) => {
+        return [
+          c.docs.map((doc) => doc.data()),
+          u.docs.map((doc) => doc.data()),
+        ];
       });
-    res.status(200).json({success: true, contests, punchlines, users});
+
+    const updatedPunchlines = punchlines.map((punchline) => {
+      const matchingContest = contestsDetails.find(
+        (contest) => contest.id === punchline.contestId
+      );
+      const matchingUser = usersDetails.find(
+        (contest) => contest.id === punchline.userId
+      );
+      return {
+        ...punchline,
+        contest: matchingContest || null,
+        user: matchingUser || null,
+      };
+    });
+
+    res.status(200).json({
+      success: true,
+      contests,
+      punchlines: updatedPunchlines,
+      users,
+    });
   } catch (error) {
     console.error("Error fetching data: ", error);
     res.status(500).json({
