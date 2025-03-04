8d857fd993fe4a0c230b66f605a71b2e075f04a7
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-04 21:15:35 +0900

    マイページ

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,20 +1,65 @@
-import { Container } from "react-bootstrap";
+import {Col, Container, Image, Row} from "react-bootstrap";
 import { useAuth } from "../AuthProvider.tsx";
+import {LoadingBlock} from "../components/Loading.tsx";
+import PunchlineCard from "../components/PunchlineCard.tsx";
+import {useUsersDetailApi} from "../hooks/usersApi.ts";
 
 function MyPage() {
-  const { loading, user } = useAuth();
+  const { loading, user: authUser } = useAuth();
+
+  const { isLoading, user, punchlines } = useUsersDetailApi(authUser?.uid);
 
   return (
-    <Container>
-      <h1>MyPage</h1>
-      {!loading && user && (
-        <>
-          <span>ようこそ</span>
-          <span>{user.displayName}</span>
-          <span>さん</span>
-        </>
-      )}
-    </Container>
+    <>
+      <Container>
+        <div className="mt-5 mb-5">
+          <Container>
+            <h1>マイページ</h1>
+          </Container>
+        </div>
+      </Container>
+
+      <div className="mb-5 py-5" style={{ backgroundColor: "#f5fff5" }}>
+        <Container>
+          <h2 className="mb-5">
+            プロフィール
+          </h2>
+          {(isLoading || loading) && <LoadingBlock />}
+          <div className="mb-5" style={{ maxWidth: 120 }}>
+            <Image
+              roundedCircle
+              src={user?.photoURL || '/default-avatar.png'}
+              className="w-100"
+            />
+          </div>
+          <div className="">
+            ニックネーム: {user?.displayName ?? "未設定"}
+          </div>
+          <div className="mb-5">
+            自己紹介: {user?.bio ?? "未設定"}
+          </div>
+        </Container>
+      </div>
+
+      <div className="mb-5 py-5" style={{ backgroundColor: "#f5fff5" }}>
+        <Container>
+          <h2 className="mb-5">
+            回答一覧
+          </h2>
+          {isLoading && <LoadingBlock />}
+          <Row xs={1} sm={2} md={4} className="g-4">
+            {punchlines?.map((p) => (
+              <Col key={p.id}>
+                <PunchlineCard punchline={p} showUser={false} />
+              </Col>
+            ))}
+          </Row>
+          {punchlines?.length === 0 &&
+            <div>回答はありません</div>
+          }
+        </Container>
+      </div>
+    </>
   );
 }
 
