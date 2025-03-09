2d89db77c25cfc6d1eb4f06bb2c4119d3ab0c596
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-09 12:00:32 +0900

    fmt

--- contracts/script/GetBalance.s.sol
+++ contracts/script/GetBalance.s.sol
@@ -12,7 +12,7 @@ contract GetBalanceScript is Script {
         addresses[0] = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
         addresses[1] = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
         addresses[2] = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
-        
+
         for (uint256 i = 0; i < addresses.length; i++) {
             address targetAddress = addresses[i];
             uint256 balance = targetAddress.balance;
