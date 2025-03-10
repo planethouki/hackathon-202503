f23311d4a03444ef3f2fba5fcbafa2cb0f0c892d
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-11 08:57:41 +0900

    フッターリンク整備

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,6 +2,19 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Footer: React.FC = () => {
+  function Anchor({ href, children }: React.PropsWithChildren<{href: string}>) {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noreferrer noopener"
+        className="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+      >
+        {children}
+      </a>
+    );
+  }
+
   return (
     <footer className="bg-dark text-white mt-5">
       <Container>
@@ -10,24 +23,14 @@ const Footer: React.FC = () => {
             <h5>メンバー</h5>
             <ul className="list-unstyled">
               <li>
-                <a
-                  href="https://x.com/begin_indiegame"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  className="text-white text-decoration-none"
-                >
+                <Anchor href="https://x.com/begin_indiegame">
                   初川鳳一
-                </a>
+                </Anchor>
               </li>
               <li>
-                <a
-                  href="https://x.com/planethouki"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  className="text-white text-decoration-none"
-                >
+                <Anchor href="https://x.com/planethouki">
                   planet★箒星
-                </a>
+                </Anchor>
               </li>
             </ul>
           </Col>
@@ -35,17 +38,27 @@ const Footer: React.FC = () => {
             <h5>リンク</h5>
             <ul className="list-unstyled">
               <li>
-                <a href="#" className="text-white text-decoration-none">
-                  未定
-                </a>
+                <Anchor href="https://hackathon-2025.nemtus.com">
+                  NEMTUS Hackathon 2025
+                </Anchor>
+              </li>
+              <li>
+                <Anchor href="https://github.com/planethouki/hackathon-202503">
+                  GitHub
+                </Anchor>
               </li>
             </ul>
           </Col>
           <Col md={4} className="text-center">
-            <h5>フォロー</h5>
-            <a href="#" className="text-white text-decoration-none mx-2">
-              未定
-            </a>
+            <h5>未定</h5>
+            <ul className="list-unstyled">
+              <li>
+                利用規約（未実装）
+              </li>
+              <li>
+                プライバシーポリシー（未実装）
+              </li>
+            </ul>
           </Col>
         </Row>
         <Row>
