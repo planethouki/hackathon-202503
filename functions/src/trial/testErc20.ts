29e9a9a52bfb188f56ed14ff836eee21727fb079
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-10 21:15:35 +0900

    Revert "use secret"
    This reverts commit ab796b524373cdfe5bd057ea9239d8aeaf9de7f1.

--- functions/src/trial/testErc20.ts
+++ functions/src/trial/testErc20.ts
@@ -4,6 +4,7 @@ import {defineSecret} from "firebase-functions/params";
 import {ethers} from "ethers";
 import {erc20Abi} from "../abi";
 
+const privateKey = defineSecret("ETH_PRIVATE_KEY");
 const rpcUrl = defineSecret("ETH_RPC_URL");
 const erc20Address = defineSecret("ETH_ERC20_ADDRESS");
 const recipient = defineSecret("ETH_TEST_RECEIVE_ADDRESS");
@@ -12,8 +13,7 @@ const app = express();
 
 app.get("/", async (req, res) => {
   const provider = new ethers.JsonRpcProvider(rpcUrl.value());
-  const wallet =
-    new ethers.Wallet(process.env.ETH_PRIVATE_KEY as string, provider);
+  const wallet = new ethers.Wallet(privateKey.value(), provider);
   const contract = new ethers.Contract(
     erc20Address.value(),
     erc20Abi,
@@ -31,6 +31,4 @@ app.get("/", async (req, res) => {
   });
 });
 
-export const eth = onRequest({
-  secrets: ["ETH_PRIVATE_KEY"],
-}, app);
+export const eth = onRequest(app);
