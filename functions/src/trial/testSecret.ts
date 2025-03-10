90d31578325c4f22cf94e61b2bacc0bdeaa2d3c7
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-10 21:16:09 +0900

    secret manager client test

--- functions/src/trial/testSecret.ts
+++ functions/src/trial/testSecret.ts
@@ -1,4 +1,8 @@
 import {onRequest} from "firebase-functions/v2/https";
+import {defineSecret} from "firebase-functions/params";
+import {SecretManagerServiceClient} from "@google-cloud/secret-manager";
+
+const secretMangerPrefix = defineSecret("SECRET_MANAGER_PREFIX");
 
 export const testSecret = onRequest({
   secrets: ["TEST_SECRET"],
@@ -7,3 +11,15 @@ export const testSecret = onRequest({
     secret: process.env.TEST_SECRET,
   });
 });
+
+export const testSecretClient = onRequest(
+  async (request, response) => {
+    const client = new SecretManagerServiceClient();
+    const name = `${secretMangerPrefix.value()}/TEST_SECRET/versions/latest`;
+    const [version] = await client.accessSecretVersion({name});
+    const payload = version.payload?.data?.toString();
+    response.json({
+      secret: payload,
+    });
+  }
+);
