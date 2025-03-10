709863e40e03d9b616284084cde5a7271d3146fa
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-10 22:03:34 +0900

    defineString, defineSecret の試し

--- functions/src/trial/testErc20.ts
+++ functions/src/trial/testErc20.ts
@@ -1,13 +1,13 @@
 import * as express from "express";
 import {onRequest} from "firebase-functions/v2/https";
-import {defineSecret} from "firebase-functions/params";
+import {defineSecret, defineString} from "firebase-functions/params";
 import {ethers} from "ethers";
 import {erc20Abi} from "../abi";
 
 const privateKey = defineSecret("ETH_PRIVATE_KEY");
-const rpcUrl = defineSecret("ETH_RPC_URL");
-const erc20Address = defineSecret("ETH_ERC20_ADDRESS");
-const recipient = defineSecret("ETH_TEST_RECEIVE_ADDRESS");
+const rpcUrl = defineString("ETH_RPC_URL");
+const erc20Address = defineString("ETH_ERC20_ADDRESS");
+const recipient = defineString("ETH_TEST_RECEIVE_ADDRESS");
 
 const app = express();
 
@@ -31,4 +31,6 @@ app.get("/", async (req, res) => {
   });
 });
 
-export const eth = onRequest(app);
+export const eth = onRequest({
+  secrets: [privateKey],
+}, app);
