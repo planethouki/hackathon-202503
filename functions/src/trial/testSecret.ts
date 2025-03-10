818edf55a74c733052a765e9a604ee5772601475
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-11 08:43:06 +0900

    functions using params projectId

--- functions/src/trial/testSecret.ts
+++ functions/src/trial/testSecret.ts
@@ -1,8 +1,6 @@
 import {onRequest} from "firebase-functions/v2/https";
-import {defineSecret} from "firebase-functions/params";
+import {projectID} from "firebase-functions/params";
 import {SecretManagerServiceClient} from "@google-cloud/secret-manager";
-
-const secretMangerPrefix = defineSecret("SECRET_MANAGER_PREFIX");
 
 export const testSecret = onRequest({
   secrets: ["TEST_SECRET"],
@@ -15,7 +13,8 @@ export const testSecret = onRequest({
 export const testSecretClient = onRequest(
   async (request, response) => {
     const client = new SecretManagerServiceClient();
-    const name = `${secretMangerPrefix.value()}/TEST_SECRET/versions/latest`;
+    const name =
+      `projects/${projectID.value()}/secrets/TEST_SECRET/versions/latest`;
     const [version] = await client.accessSecretVersion({name});
     const payload = version.payload?.data?.toString();
     response.json({
