fe7d958ec870ff138fa350d1b2f0ddeac29eb304
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-11 21:58:30 +0900

    引数必須

--- functions/src/ethUtils.ts
+++ functions/src/ethUtils.ts
@@ -5,15 +5,15 @@ import {erc20Abi} from "./abi";
 const privateKey = defineSecret("ETH_PRIVATE_KEY");
 const rpcUrl = defineString("ETH_RPC_URL");
 const erc20Address = defineString("ETH_ERC20_ADDRESS");
-const recipient = defineString("ETH_TEST_RECEIVE_ADDRESS");
 
 interface Result {
   from: string;
   hash: string;
   to: string;
+  recipient: string;
 }
 
-export const transfer = async (recipientAddress?: string): Promise<Result> => {
+export const transfer = async (recipientAddress: string): Promise<Result> => {
   const provider = new ethers.JsonRpcProvider(rpcUrl.value());
   const wallet = new ethers.Wallet(privateKey.value(), provider);
   const contract = new ethers.Contract(
