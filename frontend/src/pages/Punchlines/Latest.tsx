bd80ae6c31f92c5bf6130c2f3937f414d8c3155b
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-12 08:50:48 +0900

    use component

--- frontend/src/pages/Punchlines/Latest.tsx
+++ frontend/src/pages/Punchlines/Latest.tsx
@@ -1,8 +1,8 @@
 import {useState, useMemo} from "react";
-import {Container, Row, Col, Card, Pagination} from "react-bootstrap";
-import {Link} from "react-router";
+import {Container, Row, Col, Pagination} from "react-bootstrap";
 import {usePunchlinesLatestApi} from "../../hooks/punchlinesApi.ts";
 import {LoadingBlock} from "../../components/Loading.tsx";
+import PunchlineCard from "../../components/PunchlineCard.tsx";
 
 function PunchlinesLatest() {
   const [pageNumber, setPageNumber] = useState(1);
@@ -59,32 +59,7 @@ function PunchlinesLatest() {
           <Row xs={1} sm={2} md={4} className="g-4 mb-5">
             {punchlines?.map((p) => (
               <Col key={p.id}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>
-                      <span>回答: </span>
-                      <Link to={`/punchlines/${p.id}`}>
-                        {p.title}
-                      </Link>
-                    </Card.Title>
-                  </Card.Body>
-                  <iframe src={p.url}
-                          style={{ aspectRatio: 9/16, width: "100%" }}
-                          frameBorder="0"
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                          referrerPolicy="strict-origin-when-cross-origin"
-                          allowFullScreen></iframe>
-                  <Card.Footer>
-                    {p.contest && (
-                      <div className="mb-3">
-                        <span>お題: </span>
-                        <Link to={`/contests/${p.contest.id}`}>
-                          {p.contest.title}
-                        </Link>
-                      </div>
-                    )}
-                  </Card.Footer>
-                </Card>
+                <PunchlineCard punchline={p} />
               </Col>
             ))}
           </Row>
