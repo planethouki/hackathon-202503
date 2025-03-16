560cd16d5745403111805a6be8a4afd880d93568
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-16 23:18:33 +0900

    投稿 コンパクト

--- frontend/src/pages/Punchline/Post/Detail.tsx
+++ frontend/src/pages/Punchline/Post/Detail.tsx
@@ -2,7 +2,6 @@ import {useEffect, useState, FormEvent, useMemo} from "react";
 import {Container, Form, Button, Breadcrumb} from "react-bootstrap";
 import {useParams, useNavigate, Link} from "react-router";
 import {usePunchlinePostContestDetailApi} from "../../../hooks/punchlinePostApi.ts";
-import {LoadingBlock} from "../../../components/Loading.tsx";
 import {useTitleYouTubeContext} from "../../../contexts/TitleYouTubeContext";
 import {ContestImage} from "../../../components/ContestImage.tsx";
 
@@ -15,7 +14,7 @@ function PunchlinePostDetail() {
 
   const { title, setTitle, youTubeUrl, setYouTubeUrl } = useTitleYouTubeContext();
 
-  const {isLoading, contest} = usePunchlinePostContestDetailApi(id);
+  const {contest} = usePunchlinePostContestDetailApi(id);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -67,78 +66,67 @@ function PunchlinePostDetail() {
         </Container>
       </div>
 
-      <div className="mb-1 py-5" style={{ backgroundColor: "#f5fff5" }}>
+      <div className="mb-5 py-5" style={{ backgroundColor: "#f5fff5" }}>
         <Container>
-          {isLoading && <LoadingBlock />}
-          {contest && <>
-            <h2 className="mb-5">
-              {contest.title}
-            </h2>
-            <div style={{ maxWidth: 400 }}>
-              <ContestImage
-                fileName={contest.imageName}
-                alt={contest.title}
-              />
+          <h2 className="mb-3">
+            投稿
+          </h2>
+          {contest &&
+            <div className="mb-3">
+              <div className="mb-3">
+                お題: {contest.title}
+              </div>
+              <div style={{ maxWidth: 200 }}>
+                <ContestImage
+                  fileName={contest.imageName}
+                  alt={contest.title}
+                />
+              </div>
             </div>
-          </>}
+          }
+          {canPost &&
+            <Form onSubmit={onSubmit} onChange={onChange} className="mb-3">
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>タイトル</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="例：ふとんが吹っ飛んだww"
+                  required
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>YouTube ShortsのURL</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="https://youtube.com/shorts/M0xy9bGhn4Y?feature=shared"
+                  required
+                  pattern="https://.+"
+                  value={youTubeUrl}
+                  onChange={(e) => setYouTubeUrl(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Check
+                className="mb-3"
+                type="checkbox"
+                id="exampleForm.ControlCheckbox1"
+                label="私はこの動画に関するすべての権利を保有しています。"
+                required
+                checked={isChecked}
+                onChange={(e) => setIsChecked(e.target.checked)}
+              />
+              <Button variant="primary" type="submit" disabled={!validated}>確認</Button>
+            </Form>
+          }
+          {!canPost &&
+            <div className="mb-5">
+              投稿期限を過ぎています。
+            </div>
+          }
         </Container>
       </div>
-
-      {canPost ? (
-        <>
-          <div className="mb-5 py-5" style={{ backgroundColor: "#f5fff5" }}>
-            <Container>
-              <h2 className="mb-5">
-                投稿
-              </h2>
-              <Form onSubmit={onSubmit} onChange={onChange}>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                  <Form.Label>タイトル</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={3}
-                    placeholder="例：ふとんが吹っ飛んだww"
-                    required
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                  <Form.Label>YouTube ShortsのURL</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="https://youtube.com/shorts/M0xy9bGhn4Y?feature=shared"
-                    required
-                    pattern="https://.+"
-                    value={youTubeUrl}
-                    onChange={(e) => setYouTubeUrl(e.target.value)}
-                  />
-                </Form.Group>
-                <Form.Check
-                  className="mb-3"
-                  type="checkbox"
-                  id="exampleForm.ControlCheckbox1"
-                  label="私はこの動画に関するすべての権利を保有しています。"
-                  required
-                  checked={isChecked}
-                  onChange={(e) => setIsChecked(e.target.checked)}
-                />
-                <Button variant="primary" type="submit" disabled={!validated}>確認</Button>
-              </Form>
-            </Container>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="mb-5 py-5" style={{ backgroundColor: "#f5fff5" }}>
-            <Container>
-              <div className="mb-5">
-                投稿期限を過ぎています。
-              </div>
-            </Container>
-          </div>
-        </>
-      )}
     </>
   );
 }
