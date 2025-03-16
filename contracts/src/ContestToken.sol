7e593f4089be4521fe62731882ffac7e9dcfa570
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-16 15:12:06 +0900

    contract

--- contracts/src/ContestToken.sol
+++ contracts/src/ContestToken.sol
@@ -6,16 +6,20 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ContestToken is ERC721, Ownable {
-    string private baseURI_;
+    string private baseURI;
 
     constructor() ERC721("GloryToken", "GT") Ownable(msg.sender) {}
 
     function _baseURI() internal view override returns (string memory) {
-        return baseURI_;
+        return baseURI;
     }
 
-    function setBaseURI(string memory baseURI) public onlyOwner {
-        baseURI_ = baseURI;
+    function setBaseURI(string memory newBaseURI) public onlyOwner {
+        baseURI = newBaseURI;
+    }
+
+    function getBaseURI() public view returns (string memory) {
+        return baseURI;
     }
 
     function mint(address to, uint256 tokenId) public onlyOwner {
