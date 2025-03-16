fd6cfc74e649fb6884bba8c23626e03ba9c15027
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-16 15:24:40 +0900

    poll token erc20 permit constructor

--- contracts/src/PollToken.sol
+++ contracts/src/PollToken.sol
@@ -6,7 +6,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract PollToken is ERC20Permit {
-    constructor(uint256 initialSupply) ERC20("Poll", "POLL") {
+    constructor(uint256 initialSupply) ERC20("Poll", "POLL") ERC20Permit("Poll") {
         _mint(msg.sender, initialSupply);
     }
 }
