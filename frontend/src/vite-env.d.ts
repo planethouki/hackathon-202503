1115e4114c210438596c0e7bd2ef6c0b507a6b95
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-21 10:37:21 +0900

    vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,31 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: boolean;
+    // eslint-disable-next-line
+    request: (request: { method: string; params?: any[] }) => Promise<any>;
+    // eslint-disable-next-line
+    on: (eventName: string, callback: (...args: any[]) => void) => void;
+    // eslint-disable-next-line
+    removeListener: (eventName: string, callback: (...args: any[]) => void) => void;
+  };
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly VITE_AUTH_DOMAIN: string;
+  readonly VITE_PROJECT_ID: string;
+  readonly VITE_STORAGE_BUCKET: string;
+  readonly VITE_MESSAGING_SENDER_ID: string;
+  readonly VITE_APP_ID: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_BLOCK_EXPLORER_ADDRESS: string;
+  readonly VITE_BLOCK_EXPLORER_TOKEN: string;
+  readonly VITE_POLL_TOKEN: string;
+  readonly VITE_MEASUREMENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
