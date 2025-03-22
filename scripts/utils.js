97cf2a5f01273879f2c7b2f3978625ec7e346fc1
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-23 08:29:28 +0900

    utils

---
+++ scripts/utils.js
@@ -0,0 +1,13 @@
+function generateRandomString() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 20;
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return result;
+}
+
+module.exports = generateRandomString
