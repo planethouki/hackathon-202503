3932353ce056037ac1e931288efca33256bb8965
planethouki <26595148+planethouki@users.noreply.github.com>
2025-03-23 08:30:05 +0900

    utils

--- scripts/generateRandomString.js
+++ scripts/generateRandomString.js
@@ -1,18 +1,6 @@
-function generateRandomString() {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const length = 20;
-  let result = '';
-
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-
-  return result;
-}
+const {generateRandomString} = require('utils');
 
 for (let i = 0; i < 32; i++) {
   const id = generateRandomString()
   console.log(id)
 }
-
-module.exports = generateRandomString
